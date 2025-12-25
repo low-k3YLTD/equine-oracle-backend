7a3eaa16f92ee5d86dbd4ef3a939cae2ac89c7f7
silencemerchant <silencemerchant@gmail.com>
2025-12-25 17:21:59 +1300

    Change server listen address to '0.0.0.0'

--- src/index.ts
+++ src/index.ts
@@ -238,6 +238,6 @@ app.post('/api/predict_streak', (req: Request, res: Response) => {
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on http://0.0.0.0:${PORT}`);
 });
